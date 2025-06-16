8b037a5386f1f8918a651d15d2a967f0324ebde5
juanjoseexpositogonzalez <jjeg1979@gmail.com>
2025-06-16 13:11:35 +0200

    7. main fixed

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import streamlit as st
 
 from src.ai_chatbot.chatbot import initialize_messages, interaction, show_messages
 
-
-def main():
-    st.write("AI Chatbot Interface")
-    ai_role = "Sherlock Holmes"
-    initialize_messages(ai_role)
-    show_messages()
-    interaction()
-
-
 if __name__ == "__main__":
-    main()
+    st.set_page_config(page_title="AI Chatbot", page_icon=":robot_face:")
+    ai_role = "Sherlock Holmes"
+    st.title("AI Chatbot")
+    st.write("This is a simple AI chatbot interface using Streamlit.")
+    initialize_messages(ai_role)
+    interaction()
+    show_messages()
--- src/ai_chatbot/chatbot.py
+++ src/ai_chatbot/chatbot.py
@@ -10,23 +10,35 @@ def initialize_messages(ai_role: str) -> None:
         ]
 
 
+def handle_chat_message() -> None:
+    """Handles the chat message by appending it to the session state."""
+    user_input = st.session_state.get("user_input", "")
+
+    if user_input and user_input != "":
+        # st.chat_message("user").markdown(user_input)
+        st.session_state.messages.append({"role": "user", "content": user_input})
+
+        response = generate_response(user_input)
+
+        # st.chat_message("assistant").markdown(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+
 def show_messages() -> None:
     """Displays the messages in the chat."""
     for message in st.session_state.messages:
         if message["role"] == "user":
-            st.markdown(f"**User:** {message['content']}")
+            st.chat_message("user").markdown(f"**User:** {message['content']}")
         elif message["role"] == "assistant":
-            st.markdown(f"**Assistant:** {message['content']}")
+            st.chat_message("assistant").markdown(
+                f"**Assistant:** {message['content']}"
+            )
 
 
 def interaction():
     """Defines the interaction with the user with streamlit."""
-    st.title("AI Chatbot")
-    st.write("This is a simple AI chatbot interface using Streamlit.")
-    user_input = st.text_input("Type your message here:")
-    if user_input and user_input != "":
-        st.chat_message("user").markdown(user_input)
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        response = generate_response(user_input)
-        st.chat_message("assistant").markdown(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    # st.title("AI Chatbot")
+    # st.write("This is a simple AI chatbot interface using Streamlit.")
+    st.chat_input(
+        "Type your message here:...", key="user_input", on_submit=handle_chat_message
+    )
