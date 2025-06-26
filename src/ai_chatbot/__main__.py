1b0cdb091624c27c6d785031dedf7a43aee3695c
juanjoseexpositogonzalez <jjeg1979@gmail.com>
2025-06-26 13:05:09 +0200

    R2. Ruff formatted

--- src/ai_chatbot/__main__.py
+++ src/ai_chatbot/__main__.py
@@ -1,12 +1,14 @@
+from typing import Final
+
 import streamlit as st
 
 from src.ai_chatbot.chatbot import initialize_messages, interaction, show_messages
 
 if __name__ == "__main__":
     st.set_page_config(page_title="AI Chatbot", page_icon=":robot_face:")
-    ai_role = "Sherlock Holmes"
+    AI_ROLE: Final[str] = "Sherlock Holmes"
     st.title("AI Chatbot")
     st.write("This is a simple AI chatbot interface using Streamlit.")
-    initialize_messages(ai_role)
+    initialize_messages(AI_ROLE)
     interaction()
     show_messages()
